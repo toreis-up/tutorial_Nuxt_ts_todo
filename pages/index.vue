fe856fcba1cc5b24fea0dd3e4d490aeeb8f81045
tosup_msi <j20440@inc.kisarazu.ac.jp>
2022-06-12 16:24:44 +0900

    ちょっとおもしろくした

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,40 @@
 <template>
   <v-layout>
     <section>
-      <v-row> <h1>Nuxt-Todo-App</h1></v-row>
-      <v-row>
-        <h3>
-          <v-form @submit.prevent>
-            Add task: <v-text-field v-model="task"></v-text-field>
-            <v-btn @click="addTask">submit</v-btn>
-          </v-form>
-        </h3>
-      </v-row>
-      <v-row>
-        <v-data-table :headers="headers" :items="tasks">
-          <template v-slot:item.name="props">
-            <v-edit-dialog :return-value.sync="props.item.name"
-              >{{ props.item.name }}
-              <template v-slot:input>
-                <v-text-field
-                  v-model="props.item.name"
-                  label="Edit"
-                  single-line
-                  counter
-                ></v-text-field>
-              </template>
-            </v-edit-dialog>
-          </template>
-          <template v-slot:item.isDone="{ item }">
-            <v-simple-checkbox v-model="item.isDone"></v-simple-checkbox>
-          </template>
-          <template v-slot:item.actions="{ item }">
-            <v-icon small class="mr-2" @click="editTask(item)">
-              mdi-pencil
-            </v-icon>
-            <v-icon small @click="removeTask(item)"> mdi-delete </v-icon>
-          </template>
-        </v-data-table>
-      </v-row>
+      <v-container>
+        <v-row> <h1>Nuxt-Todo-App</h1></v-row>
+        <v-row>
+          <h3>
+            <v-form @submit.prevent>
+              Add task: <v-text-field v-model="task"></v-text-field>
+              <v-btn :disabled="task === ''" @click="addTask">submit</v-btn>
+            </v-form>
+          </h3>
+        </v-row>
+        <v-row>
+          <v-data-table :headers="headers" :items="tasks">
+            <template v-slot:item.name="props">
+              <v-edit-dialog :return-value.sync="props.item.name"
+                >{{ props.item.name }}
+                <template v-slot:input>
+                  <v-text-field
+                    v-model="props.item.name"
+                    label="Edit"
+                    single-line
+                    counter
+                  ></v-text-field>
+                </template>
+              </v-edit-dialog>
+            </template>
+            <template v-slot:item.isDone="{ item }">
+              <v-simple-checkbox v-model="item.isDone"></v-simple-checkbox>
+            </template>
+            <template v-slot:item.actions="{ item }">
+              <v-icon small @click="removeTask(item)"> mdi-delete </v-icon>
+            </template>
+          </v-data-table>
+        </v-row>
+      </v-container>
     </section>
   </v-layout>
 </template>
